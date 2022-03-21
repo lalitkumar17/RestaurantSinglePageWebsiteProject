29f2dcf98bb084906c8ff8aa2710e15bf1fd11ea
lalitkumar17 <lalitkumar1736069@gmail.com>
2022-03-22 02:27:29 +0530

    add navHamburger menu and its click functionality

--- restaurant-web-app/src/components/Navigation/Navigation.jsx
+++ restaurant-web-app/src/components/Navigation/Navigation.jsx
@@ -1,42 +1,82 @@
-import React from 'react'
-import './Navigation.css'
-import Logo from '../../constant/Image'
+import React from "react";
+import "./Navigation.css";
+import Logo from "../../constant/Image";
+import { FaBars, FaToggleOn } from "react-icons/fa";
 
-  const Navigation = () => {
+const Navigation = () => {
+  const [navswitch, setNavwitch] = React.useState(false);
   return (
-    <nav className='nav'>
-      <div className='nav__image'>
-      <img src={Logo.navlogo} alt="hay" />
+    <nav className="nav">
+      <div className="nav__image">
+        <img src={Logo.navlogo} alt="hay" />
       </div>
-      
-      <ul className='nav__center-menu'>
+      <ul className="nav__center-menu">
         <li>
-         <a href="#home">Home</a>
+          <a href="#home">Home</a>
         </li>
         <li>
-         <a href="#home">Page</a>
+          <a href="#home">Page</a>
         </li>
         <li>
-         <a href="#home">ContactUs</a>
+          <a href="#home">ContactUs</a>
         </li>
         <li>
-         <a href="#home">Blog</a>
+          <a href="#home">Blog</a>
         </li>
       </ul>
 
-        <ul className='nav__right-menu'>
-          <li>
-            <a href="#home">LogIn/Registration</a>
-          </li>
-          <li><div className='nav__right-menu__verticle-line'></div></li>
-          <li>
-            <a href="#home">BookTable</a>
-          </li>
-        </ul>
+      <ul className="nav__right-menu">
+        <li>
+          <a href="#home">LogIn/Registration</a>
+        </li>
+        <li>
+          <div className="nav__right-menu__verticle-line"></div>
+        </li>
+        <li>
+          <a href="#home">BookTable</a>
+        </li>
+      </ul>
 
-
+      <div className="nav__small-screen">
+        <FaBars
+          className="hamburger-menu"
+          onClick={() => {
+            if (navswitch) {
+              setNavwitch(false);
+            } else {
+              setNavwitch(true);
+            }
+          }}
+        />
+        {navswitch && (
+          <ul className="nav__small-screen__overlay">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#home">Page</a>
+            </li>
+            <li>
+              <a href="#home">ContactUs</a>
+            </li>
+            <li>
+              <a href="#home">Blog</a>
+            </li>
+            <li>
+              <ul className="nav__small-screen__overlay__loginAndbooktable">
+                <li>
+                  <a href="#home">LogIn/Registration</a>
+                </li>
+                <li>
+                  <a href="#home">BookTable</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navigation;
