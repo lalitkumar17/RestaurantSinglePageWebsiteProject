634d5dc168293b59b3bc4c51c64cdf73693e8734
lalitkumar17 <lalitkumar1736069@gmail.com>
2022-03-29 23:49:48 +0530

    remove margin from imagewrapper  & refractor code

--- restaurant-web-app/src/container/OurBelieves/OurBelieves.jsx
+++ restaurant-web-app/src/container/OurBelieves/OurBelieves.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./OurBelieves.css";
 import { images } from "../../constants";
+import SubHeading from "../../components/SubHeading/SubHeading";
 
 const OurBelieves = () => {
   return (
@@ -8,26 +9,27 @@ const OurBelieves = () => {
       <div className="our-believes-container__background">
         <img src={images.bg} alt="" />
       </div>
-      <div className="our-believes-container__content container-padding-style">
+      <div className="our-believes-container__our-believes-content container-padding-style">
         <div className="image-wrapper">
           <img src={images.chef} alt="" className="image-wrapper__chef-img" />
         </div>
-        <div className="our-believes__text-content">
-          <h1>What We Believe In</h1>
-          <p className="tex-content__para">
+        <div className="ob-container__ob-text-content">
+          <h1 className="sub-header-style">
+            <SubHeading title="Chef's Word" />
+            What We Believe In
+          </h1>
+          <p className="ob-text-content__para">
             <img src={images.quote} alt="" />
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro,
             beatae perspiciatis molestiae incidunt quidem dolor, expedita fuga
             labore temporibus, illo enim eos voluptate neque dicta commodi
             explicabo. Consectetur, aspernatur obcaecati.
           </p>
-          <div className="text-content__profile">
+          <div className="ob-text-content__profile">
             <p>Lalit Kumar</p>
             <p>Chef Founder</p>
           </div>
-          <div className="image-wrapper">
-            <img src={images.sign} alt="" />
-          </div>
+          <img src={images.sign} alt="" className="ob-text-content__sign" />
         </div>
       </div>
     </div>
